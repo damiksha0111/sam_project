83183b8670a8417b909ba142a485ec0af5cd9a59
damiksha0111 <smikshadongre55@gmail.com>
2022-12-13 23:57:03 +0530

    commit

--- src/sampleproject/sampe2.java
+++ src/sampleproject/sampe2.java
@@ -4,6 +4,9 @@ public class sampe2{
 	public static void main(String a[])
 	{
 		System.out.println("Hiii");
-	
+		System.out.println("Hiii");
+		System.out.println("Hiii");
+		System.out.println("Hiii");
+		System.out.println("Hiii");
 	}
 }
--- src/sampleproject/sample1.java
+++ src/sampleproject/sample1.java
@@ -2,6 +2,6 @@ package sampleproject;
 
 public class sample1 {
 uhguhghh
-
+yvkygvkhgvhgvys
 hgchgg
 }
